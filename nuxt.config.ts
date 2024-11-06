adedc31e581426126e8c339ba57388967062783c
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-11-06 09:10:12 -0500

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,30 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  future: {
+    compatibilityVersion: 4,
+  },
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+
+  typescript: {
+    typeCheck: true,
+    strict: true,
+    shim: true,
+  },
+  modules: ['@nuxt/eslint', '@vueuse/nuxt',],
+
+    // https://eslint.nuxt.com
+    eslint: {
+      config: {
+        stylistic: {
+          quotes: 'single',
+        },
+      },
+    },
+
+    css: [
+      '@/assets/css/main.css',
+    ],
+
+    ssr: true,
+})
